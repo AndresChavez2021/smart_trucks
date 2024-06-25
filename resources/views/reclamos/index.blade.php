52eb269d9c1bd92172b2495d4bd6687b383da732
Andres Chavez <andresintel773@gmail.com>
2024-06-24 21:48:35 -0400

    mapa actualizado

--- resources/views/reclamos/index.blade.php
+++ resources/views/reclamos/index.blade.php
@@ -6,10 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Open Street Map</title>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
-        integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+
+    <link rel="stylesheet" href={{asset('css/leaflet.css')}} />
+
+    <script src = {{asset('js/leaflet.js')}}>
+        
+    </script>
 </head>
 <div class="card mt-3">
     <div class="card-header d-inline-flex">
@@ -72,11 +74,11 @@
             .bindPopup("<strong>Cliente:</strong> {{ $reclamo->cliente->name }}<br><strong>Descripción:</strong> {{ $reclamo->descripcion }}");
     @endforeach
 
-    map.on('click', onMapClick);
+    //map.on('click', onMapClick);
 
-    function onMapClick(e) {
+    /*function onMapClick(e) {
         alert("Posición: " + e.latlng);
-    }
+    }*/
 </script>
 
 
