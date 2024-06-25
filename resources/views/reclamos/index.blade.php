a319584f4cfe0dd741311294e4518c0cbe5fb16c
Andres Chavez <andresintel773@gmail.com>
2024-06-24 21:54:56 -0400

    quitar copy

--- resources/views/reclamos/index.blade.php
+++ resources/views/reclamos/index.blade.php
@@ -66,7 +66,7 @@
     let map = L.map('mi_mapa').setView([-17.78327916790587, -63.182134246564736], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution: ' <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     @foreach($reclamos as $reclamo)
